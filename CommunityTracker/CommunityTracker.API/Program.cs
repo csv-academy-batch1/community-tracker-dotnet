0bb4d5730f5686690e7f2c31bf68d258c27a4571
marlonmendoza03 <marlon.mendoza@softvision.com>
2022-07-12 14:16:16 +0800

    Updated the branch

--- CommunityTracker/CommunityTracker.API/Program.cs
+++ CommunityTracker/CommunityTracker.API/Program.cs
@@ -1,10 +1,19 @@
+using CommunityTracker.Repository.Command;
+using CommunityTracker.Repository.Data_Context;
+using CommunityTracker.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+//builder.Services.AddScoped<ICommunityCommands, CommunityCommands>;
+//builder.Services.AddScoped<ICommunityCommands, CommunityCommands>;
+builder.Services.AddDbContext<CommunityDBContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
 // Configure the HTTP request pipeline.
 
