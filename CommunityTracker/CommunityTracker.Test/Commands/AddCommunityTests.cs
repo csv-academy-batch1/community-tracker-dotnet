b15c72bd87e5bc61261e84e0b8eb78df0714d9f0
angielyn.rivera <angielyn.rivera@SVPHLP00674>
2022-07-19 13:45:08 +0800

    Merged with main. Code updated. Not yet done with tests since we did code refactoring.

--- CommunityTracker/CommunityTracker.Test/Commands/AddCommunityTests.cs
+++ CommunityTracker/CommunityTracker.Test/Commands/AddCommunityTests.cs
@@ -10,7 +10,7 @@ namespace CommunityTracker.Test.Commands
     public class AddCommunityServiceTests : CommunityTrackerBaseTest
     {
         [TestMethod]
-        public async Task HappyPath_TestAddCommunityService_AddingDuplicateValueInCommunityNameAsync()
+        public async Task AddingUniqueCommunityName_Success()
         {
             //Arrange
             var mockDatabase = CreateCommunityDatabaseAsync();
@@ -20,19 +20,23 @@ namespace CommunityTracker.Test.Commands
             {
                 CommunityName = "Enterprise .Net",
                 CommunityMgrid = 10,
-                CommunityDesc = "Test Desc Happy Path"
+                CommunityDesc = "Test Success"
             });
 
             var communities = await _serviceQueries.GetAllCommunities();
-            var communityName = communities.Where(x => x.communityid == 4).Select(y => y.communityname).FirstOrDefault();
+            var addedCommunity = communities
+                .Where(x => x.communityid == 4)
+                .Select(y => y.communityname).FirstOrDefault();
 
             //Assert
+            communities.Should().NotBeEmpty();
             communities.Count().Should().Be(4);
-            community.CommunityName.Should().Be(communityName);
+            community.CommunityName.Should().Be(addedCommunity);
+            communities.Should().OnlyHaveUniqueItems(i => i.communityname);
         }
 
         [TestMethod]
-        public async Task SadPath_TestAddCommunityService_AddingDuplicateValueInCommunityNameAsync()
+        public async Task AddingDuplicateCommunityName_ShouldReturnFailedMessage()
         {
             //Arrange
             var mockDatabase = CreateCommunityDatabaseAsync();
@@ -42,7 +46,7 @@ namespace CommunityTracker.Test.Commands
             {
                 CommunityName = "TestCommunityName1",
                 CommunityMgrid = 10,
-                CommunityDesc = "Test Desc Sad Path"
+                CommunityDesc = "Return Failed Message"
             });
 
             var communities = await _serviceQueries.GetAllCommunities();
