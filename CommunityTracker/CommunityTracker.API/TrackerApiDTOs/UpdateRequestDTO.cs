f03e78ea3fea2c30b49f3f705cdb4f366374cd2f
miguelrefuerzo <mlmrefuerzo@gmail.com>
2022-07-19 18:09:45 +0800

    Add JsonProperty in UpdateRequestDTO

--- CommunityTracker/CommunityTracker.API/TrackerApiDTOs/UpdateRequestDTO.cs
+++ CommunityTracker/CommunityTracker.API/TrackerApiDTOs/UpdateRequestDTO.cs
@@ -1,13 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 namespace CommunityTracker.API.TrackerApiDTO
 {
     public class UpdateRequestDTO
     {
         [Required]
+        [JsonProperty("communityId")]
         public int communityid { get; set; }
+        [JsonProperty("communityName")]
         public string communityname { get; set; }
         [Required]
+        [JsonProperty("communityMgrid")]
         public int? communitymgrid { get; set; }
+        [JsonProperty("communityDesc")]
         public string communitydesc { get; set; }
     }
 }
