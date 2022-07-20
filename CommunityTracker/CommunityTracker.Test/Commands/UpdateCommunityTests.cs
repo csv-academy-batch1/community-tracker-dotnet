a87ec4e99e356a2dfcf2d8c0e5d658cb3d87e992
angielyn.rivera <angielyn.rivera@SVPHLP00674>
2022-07-20 15:28:53 +0800

    Added unit test for UpdateCommunityTest_AddingNonExistingManagerId

--- CommunityTracker/CommunityTracker.Test/Commands/UpdateCommunityTests.cs
+++ CommunityTracker/CommunityTracker.Test/Commands/UpdateCommunityTests.cs
@@ -22,7 +22,7 @@ namespace CommunityTracker.Test.Commands
                 CommunityId = 2,
                 CommunityName = "Enterprise .Net",
                 CommunityMgrid = 10,
-                CommunityDesc = "Update Desc"
+                CommunityDesc = "Success - Should be able to update community details based on ID."
             });
 
             var communities = await _serviceQueries.GetAllCommunities();
@@ -33,6 +33,33 @@ namespace CommunityTracker.Test.Commands
             communities.Count().Should().Be(3);
             communities.Should().OnlyHaveUniqueItems(i => i.communityname);
             community.CommunityName.Should().Be(addedCommunity.communityname);
+        }
+
+        //Endpoint Validation
+        [TestMethod]
+        public async Task UpdatingCommunityNameWithNonExistingManagerId_ReturnFailedMessage()
+        {
+            //Arrange
+            var mockDatabase = CreateCommunityDatabaseAsync();
+
+            //Act
+            var community = await _serviceCommands.UpdateCommunity(new CommunityDTO()
+            {
+                CommunityId = 2,
+                CommunityName = "Update Community",
+                CommunityMgrid = 15,
+                CommunityDesc = "Return Failed Message - When I input a managerId that does not exist in the table and click submit."
+            });
+
+            var communities = await _serviceQueries.GetAllCommunities();
+            var addedCommunity = communities.FirstOrDefault(x => x.communityid == 2);
+
+            //Assert
+            communities.Should().NotBeEmpty();
+            communities.Count().Should().Be(3);
+            communities.Should().OnlyHaveUniqueItems(i => i.communityname);
+            //community.CommunityName.Should().Be("TestCommunityName2");
+            addedCommunity.communityname.Should().Be("TestCommunityName2");
         }
     }
 }
